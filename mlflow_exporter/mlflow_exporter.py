68475882d378e30240abb3fcdc52be3a9bbaeca9
misohu <michal.hucko@canonical.com>
2023-06-22 10:22:16 +0200

    KF-3620 refactor tests and code.

--- mlflow_exporter/mlflow_exporter.py
+++ mlflow_exporter/mlflow_exporter.py
@@ -15,26 +15,6 @@ from prometheus_client import Gauge, start_http_server
 mlflow_metric = Gauge("mlflow_metric", "MLflow metric", ["metric_name"])
 
 
-class EnvDefault(argparse.Action):
-    """
-    Argparse Action which sets the value based on environment variable and parameters.
-
-    If the value is missing use environment variable if presented otherwise use default value.
-    """
-
-    def __init__(self, envvar, required=True, default=None, **kwargs):
-        """Specify the parameters for given argument."""
-        if envvar in os.environ:
-            default = os.environ[envvar]
-        if required and default:
-            required = False
-        super(EnvDefault, self).__init__(default=default, required=required, **kwargs)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        """Call the method to set the name attribute."""
-        setattr(namespace, self.dest, values)
-
-
 def collect_metrics():
     """Collect MLflow data and update Prometheus metrics."""
     num_registered_models = len(mlflow.search_registered_models())
@@ -54,27 +34,21 @@ if __name__ == "__main__":
         "-p",
         type=int,
         help="Port on which run the exporter server server",
-        action=EnvDefault,
-        envvar="PORT",
-        default=8000,
+        default=os.getenv("PORT", 8000),
     )
     parser.add_argument(
         "--mlflowurl",
         "-u",
         type=str,
         help="MLflow server url for collecting data",
-        action=EnvDefault,
-        envvar="MLFLOW_URL",
-        default="http://localhost:5000/",
+        default=os.getenv("MLFLOW_URL", "http://localhost:5000/"),
     )
     parser.add_argument(
         "--timeout",
         "-t",
         type=int,
-        help="Timeout for pooling new metrics.",
-        action=EnvDefault,
-        envvar="TIMEOUT",
-        default=30,
+        help="Timeout for polling new metrics in seconds.",
+        default=os.getenv("TIMEOUT", 30),
     )
     args = parser.parse_args()
 
